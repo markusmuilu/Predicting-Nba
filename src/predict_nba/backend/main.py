5f23cc6956d4e5dabdc37e851e7fed6dbeb64fec
markusmuilu <muilu.markus@gmail.com>
2025-11-18 22:43:12 +0200

    Added cors

--- src/predict_nba/backend/main.py
+++ src/predict_nba/backend/main.py
@@ -4,17 +4,26 @@ Includes routes for generating predictions and updating game results.
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from predict_nba.backend.routes.predict import router as predict_router
 from predict_nba.backend.routes.update import router as update_router
 from predict_nba.utils.wait_for_model import wait_for_required_files
-
 
 wait_for_required_files()
 
 app = FastAPI(
     title="NBA Prediction API",
     version="1.0"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Register route groups
