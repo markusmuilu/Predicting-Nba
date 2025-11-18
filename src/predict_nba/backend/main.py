8bcdc0a94ec83f0a2283178e61d4a76c1655545f
markusmuilu <164879294+markusmuilu@users.noreply.github.com>
2025-11-18 22:50:15 +0200

    Implement CORS middleware for NBA Prediction API
    Added CORS middleware to enable cross-origin requests.

--- src/predict_nba/backend/main.py
+++ src/predict_nba/backend/main.py
@@ -4,6 +4,7 @@ Includes routes for generating predictions and updating game results.
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from predict_nba.backend.routes.predict import router as predict_router
 from predict_nba.backend.routes.update import router as update_router
@@ -15,6 +16,14 @@ wait_for_required_files()
 app = FastAPI(
     title="NBA Prediction API",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Register route groups
